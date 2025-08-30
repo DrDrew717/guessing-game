b79db9c439cdf941e0330615d9023096ec78d11e
DrDrew717 <andyak0717@gmail.com>
2025-08-30 13:31:45 +0600

    feat: add solution to guessing game task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = null;
+    this.max = null;
+    this.mid = null;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.mid = Math.round((this.max + this.min) / 2);
+    return this.mid;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.mid;
+  }
+
+  greater() {
+    this.min = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
